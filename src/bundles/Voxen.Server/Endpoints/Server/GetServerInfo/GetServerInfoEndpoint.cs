52d415a1e4cf79e8eaed4a43f89b31b4bdb5d40d
Mathijs Nabbe <datenenaampje@gmail.com>
2026-03-10 12:18:50 +0100

    add summaries

--- src/bundles/Voxen.Server/Endpoints/Server/GetServerInfo/GetServerInfoEndpoint.cs
+++ src/bundles/Voxen.Server/Endpoints/Server/GetServerInfo/GetServerInfoEndpoint.cs
@@ -3,14 +3,19 @@ using Voxen.Server.Interfaces;
 
 namespace Voxen.Server.Endpoints.Server.GetServerInfo;
 
+/// <summary>
+/// Endpoint to retrieve basic server information.
+/// </summary>
 public sealed class GetServerInfoEndpoint(IServerConfigurationProvider serverConfigurationProvider) : EndpointWithoutRequest<GetServerInfoResponse>
 {
+    /// <inheritdoc />
     public override void Configure()
     {
         Get("/server/info");
         AllowAnonymous();
     }
 
+    /// <inheritdoc />
     public override async Task HandleAsync(CancellationToken ct)
     {
         var server = await serverConfigurationProvider.GetAsync(ct);
--- src/bundles/Voxen.Server/Endpoints/Server/GetServerLogo/GetServerLogoEndpoint.cs
+++ src/bundles/Voxen.Server/Endpoints/Server/GetServerLogo/GetServerLogoEndpoint.cs
@@ -3,14 +3,19 @@ using Voxen.Server.Interfaces;
 
 namespace Voxen.Server.Endpoints.Server.GetServerLogo;
 
+/// <summary>
+/// Endpoint to retrieve the server's logo image.
+/// </summary>
 public sealed class GetServerLogoEndpoint(IServerConfigurationProvider serverConfigurationProvider) : EndpointWithoutRequest
 {
+    /// <inheritdoc />
     public override void Configure()
     {
         Get("/server/logo");
         AllowAnonymous();
     }
 
+    /// <inheritdoc />
     public override async Task HandleAsync(CancellationToken ct)
     {
         var server = await serverConfigurationProvider.GetAsync(ct);
