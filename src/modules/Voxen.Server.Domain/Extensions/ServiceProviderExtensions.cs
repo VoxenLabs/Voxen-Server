88a21460ec1702e804608aea4336ffe19fefcdbb
Mathijs Nabbe <datenenaampje@gmail.com>
2026-03-16 13:47:01 +0100

    add xml comments

--- src/modules/Voxen.Server.Domain/Extensions/ServiceProviderExtensions.cs
+++ src/modules/Voxen.Server.Domain/Extensions/ServiceProviderExtensions.cs
@@ -5,8 +5,14 @@ using Voxen.Server.Domain.Services;
 
 namespace Voxen.Server.Domain.Extensions;
 
+/// <summary>
+/// Provides extension methods for <see cref="IServiceProvider"/> related to Voxen database initialization.
+/// </summary>
 public static class ServiceProviderExtensions
 {
+    /// <summary>
+    /// Initializes the Voxen database by seeding default data.
+    /// </summary>
     public static async Task<IServiceProvider> UseVoxenDb(this IServiceProvider services)
     {
         using var scope = services.CreateScope();
