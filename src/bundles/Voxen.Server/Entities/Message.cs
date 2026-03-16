849703f32ce890875d398a9c81f3e79aa402f4ba
Mathijs Nabbe <datenenaampje@gmail.com>
2026-03-16 10:51:27 +0100

    simplified database relations

--- src/bundles/Voxen.Server/Entities/Message.cs
+++ src/bundles/Voxen.Server/Entities/Message.cs
@@ -1,5 +1,6 @@
 namespace Voxen.Server.Entities;
 
+
 public class Message
 {
     public Guid Id { get; set; }
@@ -7,9 +8,10 @@ public class Message
     public Guid ChannelId { get; set; }
     public Channel Channel { get; set; } = null!;
 
-    public Guid UserId { get; set; } = Guid.Empty;
+    public Guid UserId { get; set; }
     public User User { get; set; } = null!;
 
-    public string Content { get; set; } = null!;
+    public required string Content { get; set; }
+
     public DateTime CreatedAt { get; set; }
 }
--- src/bundles/Voxen.Server/Entities/User.cs
+++ src/bundles/Voxen.Server/Entities/User.cs
@@ -4,27 +4,17 @@ using Voxen.Server.Enums;
 namespace Voxen.Server.Entities;
 
 /// <summary>
-/// Represents a user entity within the system.
+/// Represents a user within the Voxen server.
 /// </summary>
 public class User : IdentityUser<Guid>
 {
     /// <summary>
-    /// Gets or sets the unique identifier of the server associated with the user.
-    /// </summary>
-    public Guid ServerId { get; set; }
-    
-    /// <summary>
-    /// Gets or sets the server associated with the user.
-    /// </summary>
-    public Server Server { get; set; } = null!;
-
-    /// <summary>
-    /// Gets or sets the role of the user within the associated server.
+    /// Gets or sets the role of the user.
     /// </summary>
     public ServerRole Role { get; set; }
 
     /// <summary>
-    /// Gets or sets the collection of messages associated with the user.
+    /// Messages sent by the user.
     /// </summary>
     public ICollection<Message> Messages { get; set; } = new List<Message>();
 }
