d38df7dfb2fcb9aaa2697ceddd43fc4bcf40e139
Mathijs Nabbe <datenenaampje@gmail.com>
2026-03-23 15:37:08 +0100

    Update ServiceCollectionExtensions.cs

--- src/modules/Voxen.Server.Channels/Extensions/ServiceCollectionExtensions.cs
+++ src/modules/Voxen.Server.Channels/Extensions/ServiceCollectionExtensions.cs
@@ -2,7 +2,13 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Voxen.Server.Channels.Extensions;
 
+/// <summary>
+/// Provides extension methods for configuring Voxen Channels services in an <see cref="IServiceCollection"/>.
+/// </summary>
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Adds and configures Voxen Channels services to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
     public static IServiceCollection AddVoxenChannels(this IServiceCollection services) => services;
 }
