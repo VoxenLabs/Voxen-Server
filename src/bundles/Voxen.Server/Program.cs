d14db727b2513773dd39961991443adada6d8bcb
Mathijs Nabbe <datenenaampje@gmail.com>
2026-03-23 15:19:35 +0100

    add channels service extensions

--- src/bundles/Voxen.Server/Program.cs
+++ src/bundles/Voxen.Server/Program.cs
@@ -4,12 +4,14 @@ using Voxen.Server.Authentication.Extensions;
 using Voxen.Server.Domain.Extensions;
 using Voxen.Server.Extensions;
 using Voxen.Server.Info.Extensions;
+using Voxen.Server.Channels.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddVoxenApiServices();
 builder.Services.AddVoxenDb();
 builder.Services.AddVoxenServerInfo();
+builder.Services.AddVoxenChannels();
 
 var jwtSettings = builder.Configuration.GetSection("Jwt");
 builder.Services.AddVoxenAuthentication(jwtSettings);
---
+++ src/modules/Voxen.Server.Channels/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,8 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Voxen.Server.Channels.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddVoxenChannels(this IServiceCollection services) => services;
+}
