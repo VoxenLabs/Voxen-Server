8f799afd66e5a4f31abad0edb57720306a48205a
Mathijs Nabbe <datenenaampje@gmail.com>
2026-03-24 12:18:41 +0100

    Update SendMessageRequest.cs

--- src/modules/Voxen.Server.Channels/Endpoints/SendMessage/SendMessageRequest.cs
+++ src/modules/Voxen.Server.Channels/Endpoints/SendMessage/SendMessageRequest.cs
@@ -1,7 +1,18 @@
 namespace Voxen.Server.Channels.Endpoints.SendMessage;
 
+/// <summary>
+/// Represents a request to send a message to a specific channel.
+/// </summary>
 public class SendMessageRequest
 {
+    /// <summary>
+    /// Gets or sets the unique identifier of the channel where the message will be sent.
+    /// </summary>
     public Guid ChannelId { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the content of the message to be sent.
+    /// </summary>
     public required string Content { get; set; }
 }
+
